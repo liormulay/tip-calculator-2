8d21c02c8c617f642368eb353573cf5a3ede7131
liorm <liormualy6@gmail.com>
2020-09-30 16:14:54 +0300

    activate clear

--- js/tip calculator results.js
+++ js/tip calculator results.js
@@ -12,6 +12,14 @@ var TipCalculatorResults = function () {
         tip = params.tip;
         document.getElementById("results").innerHTML = html;
         showTip();
+        document.getElementById("clear").addEventListener("click", clearAll);
+    }
+
+    function clearAll(){
+        document.getElementById("billInput").value = "";
+        document.getElementById("service").value = "";
+        document.getElementById("peopleAmount").value = "";
+        document.getElementById("results").innerHTML = "";
     }
 
     function showTip() {
